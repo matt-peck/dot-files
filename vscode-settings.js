8b26d23896a54e64f99551a9dc48f5fb398cdbc2
matt peck <matt@mattpeck.io>
2020-02-23 20:49:32 -0500

    Update vscode-settings.js

--- vscode-settings.js
+++ vscode-settings.js
@@ -1,19 +1,50 @@
 {
+  /**************** LINTING / FORMATTING **************/
+  "editor.formatOnSave": true,
+  "eslint.run": "onSave",
+  "eslint.validate": [
+    "javascript",
+    "javascriptreact",
+  ],
+  "javascript.preferences.quoteStyle": "single",
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true
+  },
+  "eslint.alwaysShowStatus": true,
+  "editor.autoIndent": "full",
+  "editor.detectIndentation": false,
+  "editor.useTabStops": true,
+  "editor.formatOnPaste": true,
+  "editor.matchBrackets": "always",
+  "files.insertFinalNewline": true,
+  "javascript.updateImportsOnFileMove.enabled": "always",
+  "html.format.enable": true,
+  "html.format.preserveNewLines": true,
+  "files.trimTrailingWhitespace": true,
+  // "prettier.prettierPath": "/usr/local/lib/node_modules/prettier",
+  // "editor.defaultFormatter": "esbenp.prettier-vscode",
+  // "[javascript]": {
+  //   "editor.defaultFormatter": "esbenp.prettier-vscode"
+  // },
+  // "[javascript]": {
+  //   "editor.formatOnSave": false
+  // },
+  // "prettier.disableLanguages": ["js"],
+  /*************  END LINTING / FORMATTING **************/
+  /******************  THEME / STYLING ******************/
+  "workbench.colorTheme": "Cobalt2",
   "editor.fontFamily": "Operator Mono, Menlo, Monaco, 'Courier New', monospace",
   "editor.fontSize": 14,
   "editor.lineHeight": 22,
   "editor.quickSuggestionsDelay": 30,
   "workbench.editor.tabSizing": "shrink",
-  "html.format.enable": true,
-  "html.format.preserveNewLines": true,
-  "files.trimTrailingWhitespace": true,
   "editor.tabSize": 2,
   "editor.cursorWidth": 5,
   "editor.cursorStyle": "line",
   "editor.wordWrap": "on",
   "editor.fontWeight": "400",
   "editor.cursorBlinking": "solid",
-  "editor.colorDecorators": false,
+  "editor.colorDecorators": true,
   "editor.minimap.enabled": false,
   "workbench.activityBar.visible": true,
   "workbench.sideBar.location": "left",
@@ -23,15 +54,12 @@
     100
   ],
   "editor.minimap.showSlider": "always",
-  "extensions.ignoreRecommendations": false,
-  "files.exclude": {
-    "**/.git": true,
-    "**/.svn": true,
-    "**/.hg": true,
-    "**/CVS": true,
-    "**/.DS_Store": true,
-    "**/.next": true
-  },
+  "window.zoomLevel": 0,
+  "window.closeWhenEmpty": true,
+  "editor.showFoldingControls": "always",
+  "workbench.editor.enablePreview": false,
+  "editor.parameterHints.enabled": false,
+  "editor.scrollBeyondLastLine": true,
   "workbench.editor.tabCloseButton": "right",
   "emmet.includeLanguages": {
     "javascript": "javascriptreact"
@@ -46,34 +74,25 @@
     },
   },
   "window.titleBarStyle": "custom",
-  "editor.formatOnSave": true,
-  // "[javascript]": {
-  //   "editor.formatOnSave": false
-  // },
-  "eslint.autoFixOnSave": false,
-  // "prettier.disableLanguages": ["js"],
-  "eslint.alwaysShowStatus": true,
-  "editor.autoIndent": "keep",
-  "window.closeWhenEmpty": true,
-  "editor.detectIndentation": false,
-  "files.insertFinalNewline": true,
-  "editor.showFoldingControls": "always",
-  "editor.find.seedSearchStringFromSelection": true,
-  "editor.matchBrackets": "always",
-  "workbench.editor.enablePreview": true,
-  "editor.scrollBeyondLastLine": true,
-  "editor.useTabStops": true,
-  "editor.formatOnPaste": true,
-  "window.zoomLevel": 0,
-  "editor.parameterHints.enabled": false,
-  "editor.codeActionsOnSave": {
-    "source.fixAll.eslint": true
-  },
-  "workbench.colorTheme": "Cobalt2",
+  /*************  END THEME / STYLING ************/
+  /******************  TERMINAL ******************/
   "terminal.external.osxExec": "iTerm.app",
   "terminal.integrated.shell.osx": "/bin/zsh",
   "terminal.integrated.fontFamily": "Inconsolata for Powerline",
   "terminal.integrated.fontSize": 14,
   "terminal.integrated.lineHeight": 1,
-  "terminal.integrated.cursorBlinking": true
+  "terminal.integrated.cursorBlinking": true,
+  /****************  END TERMINAL ****************/
+  /*****************  UTILITIES ******************/
+  "editor.find.seedSearchStringFromSelection": true,
+  "extensions.ignoreRecommendations": false,
+  "files.exclude": {
+    "**/.git": true,
+    "**/.svn": true,
+    "**/.hg": true,
+    "**/CVS": true,
+    "**/.DS_Store": true,
+    "**/.next": true
+  }
+  /***************  END UTILITIES ****************/
 }
